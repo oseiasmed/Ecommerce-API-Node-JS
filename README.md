8e0b1ef3b51ca16f8351bce8bda143538d6864c9
Oséias Medeiros <oseiasmed@yahoo.com.br>
2020-09-28 10:49:53 -0300

    Iniciando o projeto Ecommerce API

---
+++ README.md
@@ -0,0 +1,5 @@
+# Api de E-commerce
+
+## CRUD de Produtos
+
+Projeto de uma API de E-commerce com CRUD completo.
---
+++ database/db_version.sql
@@ -0,0 +1,40 @@
+-- 16/07/2020
+CREATE TABLE IF NOT EXISTS productImages (
+    imageId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    productId INT,
+    path VARCHAR(255),
+    FOREIGN KEY (productId) REFERENCES products (productId)
+);
+
+-- refactoring to english
+drop table pedidos;
+drop table usuarios;
+drop table imagens_produtos;
+drop table produtos;
+
+CREATE TABLE IF NOT EXISTS products (
+    productId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(45),
+    price FLOAT,
+    productImage VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    orderId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    productId INT,
+    quantity INT,
+    FOREIGN KEY (productId) REFERENCES products (productId)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    userId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100),
+    password VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS productImages (
+    imageId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    productId INT,
+    path VARCHAR(255),
+    FOREIGN KEY (productId) REFERENCES products (productId)
+);
